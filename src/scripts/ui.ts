8347b96a82d596a38ee99a13768a478188c06136
maxhu08 <contact@maxhu.dev>
2025-01-18 15:00:18 -0600

    don't show results right away

--- src/scripts/ui.ts
+++ src/scripts/ui.ts
@@ -1,6 +1,8 @@
 export const corpusTextareaEl = document.getElementById("corpus-textarea") as HTMLTextAreaElement;
 export const analyzeButtonEl = document.getElementById("analyze-button") as HTMLButtonElement;
 
-export const resultsTextEl = document.getElementById("results-text") as HTMLParagraphElement;
+export const resultsEl = document.getElementById("results") as HTMLDivElement;
+
 export const keyboardEl = document.getElementById("keyboard") as HTMLDivElement;
 export const sfbsEl = document.getElementById("sfbs") as HTMLDivElement;
+export const jsonOutputEl = document.getElementById("json-output") as HTMLParagraphElement;
--- src/scripts/utils/display-result.ts
+++ src/scripts/utils/display-result.ts
@@ -1,12 +1,13 @@
-import { resultsTextEl } from "../ui";
+import { jsonOutputEl, resultsEl } from "../ui";
 import type { analyze } from "./analyze";
 
 import { updateKeyboardVisualization } from "./keyboard";
 import { updateSFBResults } from "./sfb";
 
 export const displayResult = (result: ReturnType<typeof analyze>) => {
-  resultsTextEl.textContent = JSON.stringify(result, null, 2);
-
   updateKeyboardVisualization(result.keyFrequencies, result.textLength);
   updateSFBResults(result.sfbs);
+  jsonOutputEl.textContent = JSON.stringify(result, null, 2);
+
+  resultsEl.classList.replace("hidden", "grid");
 };
