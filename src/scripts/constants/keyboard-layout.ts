939ef2832da0e8011038c2d8b04610e93c6d188a
maxhu08 <contact@maxhu.dev>
2025-01-19 10:39:51 -0600

    change to include full keyboard layout

--- src/scripts/constants/keyboard-layout.ts
+++ src/scripts/constants/keyboard-layout.ts
@@ -1,11 +1,17 @@
 export const layouts = {
-  qwerty: `q w e r t  y u i o p [ ] \\
+  qwerty: `
+~ 1 2 3 4 5 6 7 8 9 0 - =
+q w e r t  y u i o p [ ] \\
 a s d f g  h j k l ; '    
 z x c v b  n m , . /`,
-  dvorak: `' , . p y  f g c r l / = \\
-a o e u i  d h t n s -
-; q j k x  b m w v z  `,
-  colemak: `q w f p g  j l u y ; [ ] \\
-a r s t d  h n e i o '
-z x c v b  k m , . /  `
+  dvorak: `
+~ 1 2 3 4 5 6 7 8 9 0 [ ]
+' , . p y  f g c r l / = \\
+a o e u i  d h t n s -    
+; q j k x  b m w v z`,
+  colemak: `
+~ 1 2 3 4 5 6 7 8 9 0 - =
+q w f p g  j l u y ; [ ] \\
+a r s t d  h n e i o '    
+z x c v b  k m , . /`
 };
