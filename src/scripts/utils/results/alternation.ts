c85d5b90113ff7c7d02d3a7b2bb2af3c0f120517
maxhu08 <contact@maxhu.dev>
2025-01-19 08:33:16 -0600

    fix

--- src/scripts/utils/results/alternation.ts
+++ src/scripts/utils/results/alternation.ts
@@ -13,6 +13,9 @@ export const updateAlternationResults = (
   const leftPercent = ((leftKeys / totalKeys) * 100).toFixed(2);
   const rightPercent = ((rightKeys / totalKeys) * 100).toFixed(2);
 
+  if (leftKeys === 0 || rightKeys === 0) alternationsBarEl.classList.remove("gap-2");
+  else alternationsBarEl.classList.add("gap-2");
+
   alternationsBarEl.style.gridTemplateColumns = `${leftPercent}fr ${rightPercent}fr`;
 
   leftKeysText.textContent = `Left Hand Keys: ${leftKeys} (${leftPercent}%)`;
