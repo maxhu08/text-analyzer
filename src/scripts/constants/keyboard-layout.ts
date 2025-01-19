fb729c8a7c2aaf92b42ea30709ee4df7404967bf
maxhu08 <contact@maxhu.dev>
2025-01-19 09:25:46 -0600

    add other layouts

--- src/scripts/constants/keyboard-layout.ts
+++ src/scripts/constants/keyboard-layout.ts
@@ -4,6 +4,14 @@ const qwerty = `q w e r t  y u i o p [ ] \\
  a s d f g  h j k l ; '    
   z x c v b  n m , . /`;
 
+const dvoark = `' , . p y  f g c r l / = \\
+  a o e u i  d h t n s -
+  ; q j k x  b m w v z  `;
+
+const colemak = `q w f p g  j l u y ; [ ] \\
+  a r s t d  h n e i o '
+  z x c v b  k m , . /  `;
+
 export const keyboardLayoutSplit = qwerty
   .split("\n")
   .map((line) => line.trim().replace(/\s+/g, ""));
