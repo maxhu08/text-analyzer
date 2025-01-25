c73244d66d3b13d5a1a902540d73e50bf6abcf5e
maxhu08 <contact@maxhu.dev>
2025-01-25 11:34:46 -0600

    organize

--- src/scripts/utils/modal.ts
+++ src/scripts/utils/modal.ts
@@ -1,23 +1,26 @@
 import { modalOverlayEl } from "../ui";
 
 export const openModal = (modalId: string) => {
+  modalOverlayEl.setAttribute("current-modal", modalId);
   const modal = document.getElementById(modalId) as HTMLDivElement;
+
   modal.classList.replace("hidden", "block");
   modalOverlayEl.classList.replace("hidden", "block");
 };
 
 export const handleModals = () => {
-  modalOverlayEl.addEventListener("click", () => {
-    Array.from(modalOverlayEl.children[0].children).forEach((child) => {
-      if (child.classList.contains("block")) {
-        modalOverlayEl.classList.replace("block", "hidden");
-        child.classList.replace("block", "hidden");
-      }
-    });
-  });
+  modalOverlayEl.addEventListener("click", () => closeCurrentModal());
 
   const modal = document.getElementById("result-json-modal") as HTMLDivElement;
   modal.addEventListener("click", (event) => {
     event.stopPropagation();
   });
+};
+
+export const closeCurrentModal = () => {
+  const currentModalId = modalOverlayEl.getAttribute("current-modal");
+  const currentModal = document.getElementById(currentModalId as string) as HTMLDivElement;
+
+  modalOverlayEl.classList.replace("block", "hidden");
+  currentModal.classList.replace("block", "hidden");
 };
